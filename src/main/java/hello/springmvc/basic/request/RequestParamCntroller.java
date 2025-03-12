2857319c98e211264db89623abd622b391847426
kittae <jinkitea@naver.com>
2025-03-12 14:12:51 +0900

    [7-7] HTTP 요청 파라미터 - 쿼리 파라미터, HTML Form

--- src/main/java/hello/springmvc/basic/request/RequestParamCntroller.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamCntroller.java
@@ -4,9 +4,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
 
 @Slf4j
 @RestController
@@ -20,5 +24,63 @@ public class RequestParamCntroller {
         log.info("username={}, age={}", username, age);
 
         response.getWriter().write("ok");
+    }
+
+    @ResponseBody // RestController 처럼 같은 효과, messgebody에 바로 응답 가능
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge) {
+
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+
+    @ResponseBody // RestController 처럼 같은 효과, messgebody에 바로 응답 가능
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String memberName,
+            @RequestParam int memberAge) {
+
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+
+    @ResponseBody // RestController 처럼 같은 효과, messgebody에 바로 응답 가능
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String memberName, int memberAge) {
+
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+
+    @ResponseBody // RestController 처럼 같은 효과, messgebody에 바로 응답 가능
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String memberName,
+            @RequestParam(required = false) Integer memberAge) {
+
+
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+
+    @ResponseBody // RestController 처럼 같은 효과, messgebody에 바로 응답 가능
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(name = "username", required = true, defaultValue = "guest") String username,
+            @RequestParam(name = "age", required = false, defaultValue = "-1") int age) {
+
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody // RestController 처럼 같은 효과, messgebody에 바로 응답 가능
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(
+            @RequestParam Map<String, Object> paramMap) {
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
     }
 }
